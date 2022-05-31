b1cca3d6874b09001b111352ce906737ec047123
Eric <yuukisouma2003@gmail.com>
2022-05-31 15:41:02 -0300

    Refatorando código

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,22 @@ const msgBottom = window.document.getElementById ('msgBottom')
 
 // FUNÇÃO PARA DEIXAR O RELÓGIO NO AUTOMATICO SEM PRECISAR RECARREGAR PÁGINA
 const relogio = setInterval(function time() {
-    let pegarhora = new Date()
-    let hora = pegarhora.getHours()
-    let minutos = pegarhora.getMinutes()
+    let pegarHoraCompleta = new Date().toLocaleTimeString()
+    let pegarHora = new Date().getHours()
+    msg.innerText = pegarHoraCompleta
 
-    // CONFIGURAÇÃO PARA COLOCAR O NÚMERO '0' NA FRENTE QUANDO FOR < 10 
-    msg.innerText = `Agora são ${String(hora).padStart(2, '0')}:${String(minutos).padStart(2, '0')}`
-
-    //TROCA DE IMAGENS CONFORME O HORÁRIO FORNECIDADO
-    if (hora >= 6 && hora < 12) {
+    //TROCA DE IMAGENS CONFORME O HORÁRIO FORNECIDO
+    if (pegarHora >= 6 && pegarHora < 12) {
         foto.src = 'assets/imagens/dia.jpg'
         msgBottom.innerText = 'Status: dia'
     } 
 
-    else if (hora < 18) {
+    else if (pegarHora < 18) {
         foto.src = 'assets/imagens/tarde.jpg'
         msgBottom.innerText = 'Status: tarde'
     } 
     
-    else if (hora >= 18) {
+    else if (pegarHora >= 18) {
         foto.src = 'assets/imagens/noite.jpg'
         msgBottom.innerText = 'Status: noite'
         document.body.style.background = '#011934'
